45dc7980914056a2e95d45563e5856d1562caa1e
Zaltais <a@a.a>
2021-01-08 18:10:07 +0100

    first commit

---
+++ app/src/main/java/com/cygest/easmobile/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.cygest.easmobile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.block_location)
+    }
+}
